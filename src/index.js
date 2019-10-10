22d6f9daaa267f1292fbb5a7adaabc60f8db1351
haleydek <hcd09@live.com>
2019-10-10 10:16:20 -0500

    configure redux-devtools-extension for development

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+);
 
 ReactDOM.render(
     <Provider store={store}>
