8bc72269db2b9110c955ae935ec94b729e1b7b2c
haleydek <hcd09@live.com>
2019-10-16 10:20:09 -0500

    add emails to RepPage. open channels and url links in new tab from RepPage.

--- src/components/RepPage.js
+++ src/components/RepPage.js
@@ -12,9 +12,14 @@ const RepPage = ({ match, repsData }) => {
             <h1>{rep.name}</h1>
             <p>{rep.party}</p>
             {rep.phones ? rep.phones.map(phone => <li>{phone}</li>) : ""}
+            {rep.emails ? rep.emails.map(email => <li>{email}</li>) : ""}
             {rep.address? rep.address.map(addr => <address>{Object.values(addr).filter(Boolean).join(", ")}</address>) : ""}
-            {rep.channels ? rep.channels.map(channel => <li>{channel.type}: {channel.id}</li>) : ""}
-            {rep.urls ? rep.urls.map(url => <a href={url} />) : ""}
+            {rep.channels
+                ? rep.channels.map(channel => 
+                    <li>{channel.type}: <a href={`https://${channel.type}.com/${channel.id}`} target="_blank">{channel.id}</a></li>)
+                : ""
+            }
+            {rep.urls ? rep.urls.map(url => <a href={url} target="_blank">{url}</a>) : ""}
         </div>
     )
 }
