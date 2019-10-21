1399e32f032ba3dec3081e55f39c39331119d918
haleydek <hcd09@live.com>
2019-10-21 11:31:50 -0500

    map user.badgeIds to BadgesContainer props. Pass isUsersBadge function as props to Badge component.

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Badge = (props) => {
     return (
         <div id={props.id}>
-            <p>{props.content}</p>
+            <p>{props.content} - {props.isUsersBadge}</p>
         </div>
     )
 }
--- src/containers/BadgesContainer.js
+++ src/containers/BadgesContainer.js
@@ -3,11 +3,29 @@ import { connect } from 'react-redux';
 import Badge from '../components/Badge';
 
 class BadgesContainer extends React.Component{
+    handleClick = (event) => {
+        // Need action that sends a fetch request to '/users/:id' PATCH
+        // Send user's id and badge that was clicked on (event.target.id)
+        // In Rails API,
+        //   Find or create users_badge with the user_id and badge_id
+        //   If users_badge is found, delete the record.
+        //   If users_badge is NOT found, create the record.
+    }
+
+    isUsersBadge = (badgeId) => {
+        return this.props.usersBadgeIds.includes(badgeId) ? "yes" : "no"
+    }
+
     render(){
         return (
             <div className="badges">
                 {this.props.badges.map(badge => (
-                    <Badge key={badge.id} id={badge.id} content={badge.content} />)
+                    <Badge
+                        key={badge.id}
+                        id={badge.id}
+                        content={badge.content}
+                        isUsersBadge={this.isUsersBadge(badge.id)}
+                    />)
                 )}
             </div>
         )
@@ -16,7 +34,8 @@ class BadgesContainer extends React.Component{
 
 const mapStateToProps = (state) => {
     return {
-        badges: state.badges.badges
+        badges: state.badges.badges,
+        usersBadgeIds: state.user.badgeIds
     }
 }
 
