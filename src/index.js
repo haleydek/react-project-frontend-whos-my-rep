314f83ea07b331afbf66c8de2995c1630012da83
haleydek <hcd09@live.com>
2019-10-25 10:09:42 -0500

    install node-sass. Rename .css files to .scss. Update stylesheet imports.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
-import './index.css';
+import './index.scss';
 import App from './App';
 import rootReducer from './reducers/rootReducer';
 
